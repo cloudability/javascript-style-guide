a1231b645be9cd9fd5fb2f9547d91919f7dcf99c
Huy Nguyen <huy.nguyen@cloudability.com>
2016-10-25 17:04:01 -0700

    add sub-config file for our test env

---
+++ linters/__tests__/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: false,
+
+    mocha: true,
+    jest : true
+  },
+
+  rules: {
+    'import/no-extraneous-dependencies': 'off',
+
+    'react/jsx-filename-extension': 'off',
+
+    'global-require': 'off'
+  }
+};
